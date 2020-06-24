927d43b791327fe7f9b29dc161e4b402599c00f8
Jim Knape <Jimwinthropknape@gmail.com>
2020-06-23 19:39:24 -0700

    consolidate enzyme tests to one file

--- specs/EnzymeTests/test.js
+++ specs/EnzymeTests/test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import App from '../../client/src/components/App';
 
 Enzyme.configure({ adapter: new Adapter() })
