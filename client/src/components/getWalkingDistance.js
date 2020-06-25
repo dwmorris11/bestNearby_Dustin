c1bc89bd5c70008b9c2d3cb4793ca56c772a48a7
Jim Knape <Jimwinthropknape@gmail.com>
2020-06-25 12:10:06 -0700

    Separates getWalkingDistance to component

--- client/src/components/getWalkingDistance.js
+++ client/src/components/getWalkingDistance.js
@@ -1,1 +1,21 @@
-place holder 
+const getWalkingDistance = (parentLoc, attLoc) => {
+  // distance measured in degree decimals
+  const distance = Math.abs(parentLoc.lat - attLoc.lat);
+  // approx. 5 miles
+  if (distance >= 0.07) {
+    return '45 min';
+  // approx. 3.5 miles
+  } else if (distance >= 0.05) {
+    return '30 min';
+  // approx. 2 miles
+  } else if (distance >= 0.03) {
+    return '15 min';
+  // approx. 1 mile
+  } else if (distance >= 0.01) {
+    return '10 min';
+  // less than a mile
+  }
+  return '5 min';
+};
+
+export default getWalkingDistance;
